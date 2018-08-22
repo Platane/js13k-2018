c9caeabb77d1249bd6f6c0f997a9219ec5920932
platane <me@platane.me>
2018-08-22 17:26:53 +0100

    :rocket: build script

--- .babelrc.js
+++ .babelrc.js
@@ -10,6 +10,21 @@ const presets = ['@babel/preset-flow']
 if (process.env.NODE_ENV === 'production') {
   presets.push('@babel/preset-env')
 }
+if (process.env.NODE_ENV === 'minify') {
+  presets.length = 0
+  plugins.length = 0
+
+  presets.push([
+    'babel-preset-minify',
+    {
+      mangle: {
+        topLevel: true,
+      },
+      keepClassName: false,
+      keepFnName: false,
+    },
+  ])
+}
 if (process.env.NODE_ENV === 'test') {
   plugins.push('@babel/plugin-transform-modules-commonjs')
 }
---
+++ script/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+rm -rf dist
+
+# bundle
+yarn rollup -c
+
+# minify
+NODE_ENV=minify yarn babel dist/bundle.js -o dist/index.js
+
+rm dist/bundle.js
+
+# zip
+( cd dist && zip -r bundle.zip . )
+
+# print size
+stat --printf="%s\n" ./dist/bundle.zip
