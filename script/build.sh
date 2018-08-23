59492a92766a0b80d7e05ed740af90578b054977
platane <me@platane.me>
2018-08-23 09:53:30 +0100

    :hammer: fix build

--- script/build.sh
+++ script/build.sh
@@ -10,18 +10,20 @@ sed -i 's|lib/index.js|src/index.js|' ./node_modules/babel-plugin-minify-mangle-
 # bundle
 yarn rollup --config ./script/rollup.config.js
 
+# replace constant
+sed -i "s|'idle'|4|g" dist/bundle.js
+sed -i "s|'carry'|2|g" dist/bundle.js
+sed -i "s|'activate'|3|g" dist/bundle.js
+sed -i "s|'unreachable target'||g" dist/bundle.js
+
 # minify
 NODE_ENV="mangle-properties" yarn babel dist/bundle.js -o dist/bundle0.js
 NODE_ENV="minify" yarn babel dist/bundle0.js -o dist/index.js
 
+
 rm dist/bundle.js
 rm dist/bundle0.js
 
-# replace constant
-sed -i 's|"idle"|4|' dist/index.js
-sed -i 's|"carry"|2|' dist/index.js
-sed -i 's|"activate"|3|' dist/index.js
-sed -i 's|"unreachable target"||' dist/index.js
 
 # copy index.html
 cp src/index.html dist/index.html
